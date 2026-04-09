0624c77ad24654e7b3ba2e7d887407f7f9732804
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-04-09 07:14:28 +0000

    :memo: Protection rule 追加

--- envs/waf.tf
+++ envs/waf.tf
@@ -18,20 +18,20 @@ Regional WAF Policy
 resource "oci_waf_web_app_firewall_policy" "this" {
   compartment_id = oci_identity_compartment.workload.id
   display_name   = "regional-waf-policy"
-  ### Actions
-  ## Default
+  #### Actions
+  ### Default
   actions {
     code = 0
     name = "Pre-configured Check Action"
     type = "CHECK"
   }
-  ## Default
+  ### Default
   actions {
     code = 0
     name = "Pre-configured Allow Action"
     type = "ALLOW"
   }
-  ## Default
+  ### Default
   actions {
     code = 401
     name = "Pre-configured 401 Response Code Action"
@@ -49,7 +49,22 @@ resource "oci_waf_web_app_firewall_policy" "this" {
       value = "application/json"
     }
   }
-  ## Custom
+  ### Custom
+  actions {
+    code = 403
+    name = "Custom-configured-403-response-code-action"
+    type = "RETURN_HTTP_RESPONSE"
+    body {
+      template = jsonencode({
+        code      = "403"
+        message   = "Forbidden"
+        RequestId = "$${http.request.id}"
+      })
+      text = null
+      type = "DYNAMIC"
+    }
+  }
+  ### Custom
   actions {
     code = 429
     name = "Custom-configured-429-response-code-action"
@@ -64,6 +79,7 @@ resource "oci_waf_web_app_firewall_policy" "this" {
       type = "DYNAMIC"
     }
   }
+  #### Access control
   ### Request access rules
   request_access_control {
     ### rulesの記載順序=評価順
@@ -87,7 +103,10 @@ resource "oci_waf_web_app_firewall_policy" "this" {
     }
     default_action_name = "Pre-configured Allow Action" # "Pre-configured Allow Action" or "Pre-configured 401 Response Code Action" or Custom
   }
-  ### Rate limiting
+  ### Response access rules
+  # response_access_control {
+  # }
+  #### Rate limiting
   request_rate_limiting {
     ### rulesの記載順序=評価順
     ### 上から評価さる
@@ -104,9 +123,162 @@ resource "oci_waf_web_app_firewall_policy" "this" {
       action_name = "Custom-configured-429-response-code-action"
     }
   }
-  ### Response access rules
-  # response_access_control {
-  # }
+  #### Protections
+  request_protection {
+    body_inspection_size_limit_in_bytes = 8192
+    rules {
+      action_name                = "Custom-configured-403-response-code-action"
+      condition_language         = "JMESPATH"
+      is_body_inspection_enabled = false
+      name                       = "recommend-rules"
+      type                       = "PROTECTION"
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "942270"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "942260"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "9420000"
+        version                        = 2
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "9420000"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "941140"
+        version                        = 3
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "9410000"
+        version                        = 3
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "9410000"
+        version                        = 2
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "9410000"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "9330000"
+        version                        = 2
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "9330000"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "9320001"
+        version                        = 2
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "9320001"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "9320000"
+        version                        = 2
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "9320000"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "930120"
+        version                        = 2
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "9300000"
+        version                        = 2
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "9300000"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "920390"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "920380"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "920370"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "920320"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "920300"
+        version                        = 2
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "920300"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "920280"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "911100"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "200003"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "200002"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "200001"
+        version                        = 1
+      }
+      protection_capabilities {
+        collaborative_action_threshold = 0
+        key                            = "200000"
+        version                        = 1
+      }
+    }
+  }
   defined_tags = local.common_defined_tags
 }
 

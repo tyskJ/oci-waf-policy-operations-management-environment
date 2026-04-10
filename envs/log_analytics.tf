4a128d98df77c608a65106004ce75ad00f4661c3
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-04-10 05:33:31 +0000

    :memo: LogAnalytics LogGroup追加

--- envs/log_analytics.tf
+++ envs/log_analytics.tf
@@ -9,7 +9,7 @@ resource "oci_log_analytics_namespace" "this" {
   is_onboarded   = var.loganalytics_onboard
   namespace      = var.namespace
   provisioner "local-exec" {
-    when = destroy
+    when    = destroy
     command = <<EOT
       oci log-analytics namespace offboard \
       --namespace-name ${self.namespace} \
@@ -17,4 +17,15 @@ resource "oci_log_analytics_namespace" "this" {
       --auth security_token
     EOT
   }
+}
+
+/************************************************************
+LogGroup
+************************************************************/
+resource "oci_log_analytics_log_analytics_log_group" "this" {
+  compartment_id = oci_identity_compartment.workload.id
+  display_name   = "waf-log-group"
+  description    = "For Regional WAF Log Group"
+  namespace      = var.namespace
+  defined_tags   = local.common_defined_tags
 }

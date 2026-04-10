0972e6f60e7f1d15166fd4a96d3e4d79ec9dc9ea
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-04-10 03:21:14 +0000

    :memo: 必要なためコメントアウトを外す

--- envs/iam.tf
+++ envs/iam.tf
@@ -7,10 +7,10 @@ resource "oci_identity_policy" "lgan_enable" {
   name           = "logging_analytics_automatic_service_policies"
   statements = [
     ### For Enabling Sample Logs
-    # "define tenancy sampledata as ocid1.tenancy.oc1..aaaaaaaabmtv54v5bg45j7zd2eeat4df2bwfqkmxe2yy6ecdfrc36wloe4ia",
-    # "endorse group Administrators to read loganalytics-features-family in tenancy sampledata",
-    # "endorse group Administrators to read loganalytics-resources-family in tenancy sampledata",
-    # "endorse group Administrators to read compartments in tenancy sampledata",
+    "define tenancy sampledata as ocid1.tenancy.oc1..aaaaaaaabmtv54v5bg45j7zd2eeat4df2bwfqkmxe2yy6ecdfrc36wloe4ia",
+    "endorse group Administrators to read loganalytics-features-family in tenancy sampledata",
+    "endorse group Administrators to read loganalytics-resources-family in tenancy sampledata",
+    "endorse group Administrators to read compartments in tenancy sampledata",
     ### For Enabling Log Analytics
     "allow service loganalytics to READ loganalytics-features-family in tenancy",
     "allow service loganalytics to READ compartments in tenancy",

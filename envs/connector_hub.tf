a687baf2eb5d5d55b8d1af7a40215eafb1f20f4e
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-04-11 17:42:02 +0900

    :memo: 通知用コネクタハブ追加

--- envs/connector_hub.tf
+++ envs/connector_hub.tf
@@ -1,6 +1,7 @@
 /************************************************************
 Connector Hub
 ************************************************************/
+### Logging to LogAnalytics
 resource "oci_sch_service_connector" "check_log" {
   compartment_id = oci_identity_compartment.workload.id
   display_name   = "waf-check-logging-to-loganalytics"
@@ -59,6 +60,69 @@ resource "oci_sch_service_connector" "block_log" {
     enable_formatted_messaging = false
     kind                       = "loggingAnalytics"
     log_group_id               = oci_log_analytics_log_analytics_log_group.this.id
+  }
+  defined_tags = local.common_defined_tags
+}
+
+### Logging to Notifications
+resource "oci_sch_service_connector" "check_notifications" {
+  compartment_id = oci_identity_compartment.workload.id
+  display_name   = "waf-check-logging-to-notifications"
+  state          = "ACTIVE" # ACTIVE or INACTIVE
+  source {
+    kind = "logging"
+    log_sources {
+      compartment_id = oci_logging_log_group.this.compartment_id
+      log_group_id   = oci_logging_log_group.this.id
+      log_id         = oci_logging_log.this.id
+    }
+  }
+  tasks {
+    batch_size_in_kbs = 0
+    batch_time_in_sec = 0
+    condition         = "data.action='allow' and data.requestProtection='*'"
+    kind              = "logRule"
+  }
+  target {
+    batch_rollover_size_in_mbs = 0
+    batch_rollover_time_in_ms  = 0
+    batch_size_in_kbs          = 0
+    batch_size_in_num          = 0
+    batch_time_in_sec          = 0
+    enable_formatted_messaging = true
+    kind                       = "notifications"
+    topic_id                   = oci_ons_notification_topic.this.id
+  }
+  defined_tags = local.common_defined_tags
+}
+
+resource "oci_sch_service_connector" "block_notifications" {
+  compartment_id = oci_identity_compartment.workload.id
+  display_name   = "waf-block-logging-to-notifications"
+  state          = "ACTIVE" # ACTIVE or INACTIVE
+  source {
+    kind = "logging"
+    log_sources {
+      compartment_id = oci_logging_log_group.this.compartment_id
+      log_group_id   = oci_logging_log_group.this.id
+      log_id         = oci_logging_log.this.id
+    }
+  }
+  tasks {
+    batch_size_in_kbs = 0
+    batch_time_in_sec = 0
+    condition         = "data.action='block'"
+    kind              = "logRule"
+  }
+  target {
+    batch_rollover_size_in_mbs = 0
+    batch_rollover_time_in_ms  = 0
+    batch_size_in_kbs          = 0
+    batch_size_in_num          = 0
+    batch_time_in_sec          = 0
+    enable_formatted_messaging = true
+    kind                       = "notifications"
+    topic_id                   = oci_ons_notification_topic.this.id
   }
   defined_tags = local.common_defined_tags
 }

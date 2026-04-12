ccfb352e89f85714bf2eba59d3ef8e76fef6d391
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-04-12 15:05:58 +0900

    :memo: 統合

--- envs/connector_hub.tf
+++ envs/connector_hub.tf
@@ -63,9 +63,9 @@ resource "oci_sch_service_connector" "block_log" {
 }
 
 ### Logging to Notifications
-resource "oci_sch_service_connector" "check_notifications" {
+resource "oci_sch_service_connector" "check_and_block_notifications" {
   compartment_id = oci_identity_compartment.workload.id
-  display_name   = "waf-check-logging-to-notifications"
+  display_name   = "waf-check-and-block-logging-to-notifications"
   state          = "ACTIVE" # ACTIVE or INACTIVE
   source {
     kind = "logging"
@@ -78,37 +78,7 @@ resource "oci_sch_service_connector" "check_notifications" {
   tasks {
     batch_size_in_kbs = 0
     batch_time_in_sec = 0
-    condition         = "data.action='allow' and data.requestProtection='*'"
-    kind              = "logRule"
-  }
-  target {
-    batch_rollover_size_in_mbs = 0
-    batch_rollover_time_in_ms  = 0
-    batch_size_in_kbs          = 0
-    batch_size_in_num          = 0
-    batch_time_in_sec          = 0
-    enable_formatted_messaging = true
-    kind                       = "notifications"
-    topic_id                   = oci_ons_notification_topic.this.id
-  }
-}
-
-resource "oci_sch_service_connector" "block_notifications" {
-  compartment_id = oci_identity_compartment.workload.id
-  display_name   = "waf-block-logging-to-notifications"
-  state          = "ACTIVE" # ACTIVE or INACTIVE
-  source {
-    kind = "logging"
-    log_sources {
-      compartment_id = oci_logging_log_group.this.compartment_id
-      log_group_id   = oci_logging_log_group.this.id
-      log_id         = oci_logging_log.this.id
-    }
-  }
-  tasks {
-    batch_size_in_kbs = 0
-    batch_time_in_sec = 0
-    condition         = "data.action='block'"
+    condition         = "(data.action='block' and data.clientAddr='113.*') or (data.action='allow' and data.requestProtection.matchedRules='*')"
     kind              = "logRule"
   }
   target {

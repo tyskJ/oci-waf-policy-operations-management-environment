040f1a47a23264dea9ac3ec4e9d15127fffda9f1
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-04-12 16:09:43 +0900

    :bug: Alarmをイベントルールで拾おうと考えていたが未対応のため削除

--- envs/monitoring.tf
+++ envs/monitoring.tf
@@ -1,22 +1,22 @@
-/************************************************************
-Alarm
-************************************************************/
-resource "oci_monitoring_alarm" "this" {
-  compartment_id = oci_identity_compartment.workload.id
-  display_name   = "log-analytics-storage-used-5gb-alarm"
-  is_enabled     = true
-  ### Metric description & Trigger rule
-  metric_compartment_id = var.tenancy_ocid
-  namespace             = "oci_logging_analytics"
-  query                 = "ActiveStorageUsed[15m].max() > 5" # 5GB
-  pending_duration      = "PT1M"
-  severity              = "WARNING"
-  body                  = "Log Analytics Active Storage Used greater than 5 GB. Please Check Log Analytics Storage."
-  ### Define alarm notifications
-  destinations       = [oci_ons_notification_topic.this.id]
-  notification_title = "Log Analytics Active Storage Used greater than 5 GB"
-  ## Message grouping
-  is_notifications_per_metric_dimension_enabled = false
-  ## Message Format
-  message_format = "ONS_OPTIMIZED"
-}
+# /************************************************************
+# Alarm
+# ************************************************************/
+# resource "oci_monitoring_alarm" "this" {
+#   compartment_id = oci_identity_compartment.workload.id
+#   display_name   = "log-analytics-storage-used-5gb-alarm"
+#   is_enabled     = true
+#   ### Metric description & Trigger rule
+#   metric_compartment_id = var.tenancy_ocid
+#   namespace             = "oci_logging_analytics"
+#   query                 = "ActiveStorageUsed[15m].max() > 5" # 5GB
+#   pending_duration      = "PT1M"
+#   severity              = "WARNING"
+#   body                  = "Log Analytics Active Storage Used greater than 5 GB. Please Check Log Analytics Storage."
+#   ### Define alarm notifications
+#   destinations       = [oci_ons_notification_topic.this.id]
+#   notification_title = "Log Analytics Active Storage Used greater than 5 GB"
+#   ## Message grouping
+#   is_notifications_per_metric_dimension_enabled = false
+#   ## Message Format
+#   message_format = "ONS_OPTIMIZED"
+# }

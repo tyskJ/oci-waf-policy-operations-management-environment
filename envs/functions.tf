fe879adbc38ff21b21dc3aa77f2cbba6858e9a7e
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-04-12 17:44:02 +0900

    :memo: Application追加

---
+++ envs/functions.tf
@@ -0,0 +1,23 @@
+/************************************************************
+Application
+************************************************************/
+resource "oci_functions_application" "this" {
+  compartment_id = oci_identity_compartment.workload.id
+  display_name   = "management-app"
+  subnet_ids = [
+    oci_core_subnet.private_functions.id
+  ]
+  shape               = "GENERIC_X86"
+  security_attributes = {}
+  config              = {}
+  network_security_group_ids = [
+    oci_core_network_security_group.sg_functions_app.id
+  ]
+  syslog_url = null
+  trace_config {
+    is_enabled = false
+  }
+  image_policy_config {
+    is_policy_enabled = false
+  }
+}

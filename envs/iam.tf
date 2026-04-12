da6b6290224e19ba234b041d73b110e1246b8af2
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-04-12 18:23:48 +0900

    :memo: registryログイン用auth token作成

--- envs/iam.tf
+++ envs/iam.tf
@@ -133,3 +133,16 @@ resource "oci_identity_policy" "connhub_loganalytics" {
     )
   ]
 }
+
+/************************************************************
+Auth Token
+************************************************************/
+resource "oci_identity_auth_token" "this" {
+  user_id     = var.work_user_ocid
+  description = "For OCI Container Registry Login PW"
+}
+
+resource "local_sensitive_file" "auth_token" {
+  filename = "./.key/work_user_auth_token.txt"
+  content  = oci_identity_auth_token.this.token
+}
